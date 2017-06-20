ddf432ffd508685eb13514f23bf79c29b55893c4
Kenenth Kristensen <kehk@Kenenths-MacBook-Pro.local>
2017-06-20 10:50:39 +0200

    Add route data and som documentation

--- v1/config/routes.php
+++ v1/config/routes.php
@@ -2,22 +2,29 @@
 
 require_once('Route.php');
 
+/**
+ * Dynamic params regex examples:
+ *
+ * /{personId:\d+}  //  Match numeric one or unlimited
+ *
+ * 	Regex's:
+ *	\d+ numeric value, one or unlimited digits
+ * 	.+ wildcard, anything goes, one or unlimited chars
+ */
+
 $aRoutes = array();
 
 $oRoute = new Route();
 $oRoute->setName('TestRoute');
-$oRoute->setUrl('person');
+$oRoute->setUrl('/person');
 $oRoute->setAction('Person', 'person');
 $aRoutes[$oRoute->getUrl()] = $oRoute;
 
 $oRoute = new Route();
 $oRoute->setName('TestRoute');
-$oRoute->setUrl('person/{someVal}');
+$oRoute->setUrl('/person/{someVal:\d+}');
 $oRoute->setAction('Person', 'person');
 $aRoutes[$oRoute->getUrl()] = $oRoute;
-
-
-
 
 
 ?>
