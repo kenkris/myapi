36fbed1b44cdc80738891363cb3505baae2422cf
Kenenth Kristensen <kehk@dhcp-250-130.int.stofa.net>
2017-06-27 09:42:17 +0200

    Add getter and setter to Person

--- v1/application/model/Person.php
+++ v1/application/model/Person.php
@@ -38,14 +38,41 @@ class Person{
 	/**
 	 * Getters and setters
 	 */
-	public function getId() : int{ return $this->iId; }
+	public function getId() : int { return $this->iId; }
 	public function setId(int $iId){ $this->iId = $sId; }
 
+	public function getFirstName() : string { return $this->sFirstName; }
+	public function setFirstName(string $sFirstName){ $this->sFirstName = $sFirstName; }
 
+	public function getLastName() : string { return $this->sLastName; }
+	public function setLastName(string $sLastName){ $this->sLastName = $sLastName; }
+
+	public function getEmail() : string { return $this->sEmail; }
+	public function setEmail(string $sEmail){ $this->sEmail = $sEmail; }
+
+	public function getDOB() : string { return $this->sDOB; }
+	public function setDOB(string $sDOB){ $this->sDOB = $sDOB; }
+
+	public function getPhoneNo() : string { return $this->sPhoneNo; }
+	public function setPhoneNo(string $sPhoneNo){ $this->sPhoneNo = $sPhoneNo; }
+
+	public function getCellPhoneNo() : string { return $this->sCellPhoneNo; }
+	public function setCellPhoneNo(string $sCellPhoneNo){ $this->sCellPhoneNo = $sCellPhoneNo; }
+
+	/**
+	 * [createPerson description]
+	 * @param  string $sFirstName
+	 * @param  string $sLastName
+	 * @param  string $sEmail
+	 * @param  string $sDOB
+	 * @param  string $sPhoneNo
+	 * @param  string $sCellPhoneNo
+	 * @return integer
+	 */
+	public function createPerson(string $sFirstName, string $sLastName, string $sEmail, string $sDOB = '', string $sPhoneNo = '', string $sCellPhoneNo = '') : int {
+
+	}
 
 }
 
-
 ?>
-
-
